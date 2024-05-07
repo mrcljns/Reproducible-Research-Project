956c90f49c261f61f689a9be20393dfe72375a2e
dkoterwa <koterwadominik@icloud.com>
2024-05-07 23:01:00 +0200

    Prepare a script to train all the models and convert y for covid data

--- scripts/utils.py
+++ scripts/utils.py
@@ -5,6 +5,7 @@ from typing import Union
 def load_dataset(x_path: Union[str, os.PathLike], y_path: Union[str, os.PathLike]) -> pd.DataFrame:
     X = pd.read_csv(x_path)
     y = pd.read_csv(y_path)
-    return X, y.iloc[:, 0]
+    return X, y.iloc[:, 0].tolist()
+
     
     
